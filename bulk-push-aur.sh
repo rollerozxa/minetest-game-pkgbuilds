31244c2277a304a1706b88d5038458b8642cf84f
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-11-02 17:09:57 +0100

    Fix script exclusions, add bulk-push-aur.sh

--- bulk-push-aur.sh
+++ bulk-push-aur.sh
@@ -3,7 +3,9 @@
 for f in *; do
 	if [ -d "$f" ] && [ "$f" != "_template" ] && [ "$f" != ".git" ] && [ "$f" != "TODO" ]; then
 		cd "$f"
-		makepkg --printsrcinfo > .SRCINFO
+		git add PKGBUILD .SRCINFO
+		git commit -m "Automatic package update from master Git repository"
+		git push
 		cd ..
 	fi
 done
--- bulk-update-hashes.sh
+++ bulk-update-hashes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for f in *; do
-	if [ -d "$f" ] && [ "$f" != "_template" ]; then
+	if [ -d "$f" ] && [ "$f" != "_template" ] && [ "$f" != ".git" ] && [ "$f" != "TODO" ]; then
 		cd "$f"
 		updpkgsums
 		cd ..
