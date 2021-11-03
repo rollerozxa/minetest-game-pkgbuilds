683e7de78730551b9d0b417496cae0afa74ea13c
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-11-03 18:32:14 +0100

    Don't try to push if there's no changes in bulk-push-aur.sh, speeding up the process. Probably easier on the AUR servers as well. :]

--- bulk-push-aur.sh
+++ bulk-push-aur.sh
@@ -4,8 +4,7 @@ for f in *; do
 	if [ -d "$f" ] && [ "$f" != "_template" ] && [ "$f" != ".git" ] && [ "$f" != "TODO" ]; then
 		cd "$f"
 		git add PKGBUILD .SRCINFO
-		git commit -m "Automatic package update from master Git repository"
-		git push
+		git commit -m "Automatic package update from master Git repository" && git push
 		cd ..
 	fi
 done
