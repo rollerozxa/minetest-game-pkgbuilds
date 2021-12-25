5a3b6053a9d582dfd31ebd06efdef257eda30ec3
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-12-25 21:43:01 +0100

    Add .gitignore to initial commit

--- TODO/init-new-repos.sh
+++ TODO/init-new-repos.sh
@@ -4,7 +4,7 @@ for f in *; do
 	if [ -d "$f" ]; then
 		cd "$f"
 		git init
-		git add PKGBUILD .SRCINFO
+		git add .gitignore PKGBUILD .SRCINFO
 		git commit -m "Initializing new package from master Git repository."
 		git remote add origin ssh://aur@aur.archlinux.org/$f.git
 		git push -u origin master
